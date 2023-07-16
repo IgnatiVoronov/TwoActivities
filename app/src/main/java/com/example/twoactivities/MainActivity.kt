57cd74661a476dc202f59ea9de5fc478ce31b128
Ignati Voronov <gntvrnv@gmail.com>
2023-07-16 23:13:33 +0300

    First commit

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.twoactivities
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var mMessageEditText: EditText? = null
+    private var mReplyHeadTextView: TextView? = null
+    private var mReplyTextView: TextView? = null
+    private val TEXT_REQUEST = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mMessageEditText = findViewById(R.id.editText)
+        mReplyHeadTextView = findViewById(R.id.textHeaderReplyTextView)
+        mReplyTextView = findViewById(R.id.textMessageReplyTextView)
+    }
+
+    fun launchSecondActivity(view: View) {
+        Log.d("TAG", "Button clicked!")
+
+        val intent = Intent(this, SecondActivity::class.java)
+        val message = mMessageEditText?.text.toString()
+        intent.putExtra("Message", message)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply: String? = data?.getStringExtra("Reply")
+                mReplyHeadTextView?.visibility = View.VISIBLE
+                mReplyTextView?.text = reply
+                mReplyTextView?.visibility = View.VISIBLE
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.twoactivities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity : AppCompatActivity() {
+
+    private var mReply: EditText? = null
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val intent = intent
+        val message = intent.extras?.getString("Message")
+        val textView: TextView = findViewById(R.id.textMessageReplyTextView)
+
+        mReply = findViewById(R.id.editText2);
+        textView.text = message.toString()
+    }
+
+    fun returnReply(view: View) {
+        val reply = mReply?.text.toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra("Reply", reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
+    }
+}
